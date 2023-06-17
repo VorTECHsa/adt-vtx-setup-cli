fa5c7811086b539f6d5fbdd6d829cf442f6d9fee
Sam Hughes <sam.hughes@vortexa.com>
2023-06-17 14:56:56 +0100

    feat: add NVM install, aws-iam-authenticator app install, structuring, version command.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+VERSION="1.0.0"
+
 # ==========================================================
 # == Args & Constants                                     ==
 # ==========================================================
 
 # -- Args
+# Provide friendly name to 1st arg (<workflow>)
 WORKFLOW=$1
 
 # -- Constants
+# Root dir of all cloned repositories
 REPOS_DIR="$HOME/workspace"
+# Dir to SSH config
 SSH_DIR="$HOME/.ssh"
+# Path to SSH key file (for id_ed25519)
 SSH_KEY_FILE="$SSH_DIR/id_ed25519"
+# Path (no trailing '/') to our GitHub SSH clone path
 GITHUB_REPOS_SSH_PREFIX="git@github.com:VorTECHsa"
 # Token that is used as a prefix for any content added to any files
 # such as .zprofile, such that we know if we should add content to them.
@@ -21,15 +28,17 @@ ADDED_BY_US_TOKEN="# -- Added by vcli"
 # == General configuration (for all workflows)            ==
 # ==========================================================
 
-# General Homebrew apps to install for any workflow
-GENERAL_HOMEBREW_CASK_APPS=("visual-studio-code" "insomnia" "cyberduck" "google-chrome")
-GENERAL_HOMEBREW_NON_CASK_APPS=("awscli" "sops")
+# General Homebrew apps to install
+GENERAL_HOMEBREW_CASK_APPS=("visual-studio-code" "insomnia" "cyberduck" "google-chrome" "aws-vpn-client")
+GENERAL_HOMEBREW_NON_CASK_APPS=("awscli" "sops" "aws-iam-authenticator" "kubectl")
 
-GENERAL_ALIASES="# General Conveniences
+# General helpful aliases
+GENERAL_ALIASES_ZSHRC="
+# Misc. Aliases
 alias c='clear'
 alias cls='clear'
 
-# Git
+# Git Aliases
 alias gs='git status'
 alias gco='git checkout'
 alias gcob='git checkout -b'
@@ -39,19 +48,69 @@ alias gm='git merge'
 alias gp='git push'
 alias gf='git fetch'
 alias gp='git push'
-alias gl='git pull'"
+alias gl='git pull'
+"
+
+# Provided the `asp` command for all terminals.
+ASP_COMMAND_ZSHRC='
+# Lists available aws profiles from ~/.aws/config
+function aws_profiles() {
+  aws --no-cli-pager configure list-profiles 2> /dev/null && return
+  [[ -r "${AWS_CONFIG_FILE:-$HOME/.aws/config}" ]] || return 1
+  grep --color=never -Eo '\''\[.*\]'\'' "${AWS_CONFIG_FILE:-$HOME/.aws/config}" | sed -E '\''s/^[[:space:]]*\[(profile)?[[:space:]]*([^[:space:]]+)\][[:space:]]*$/\2/g'\''
+}
+
+# AWS profile selection
+function asp() {
+  if [[ -z "$1" ]]; then
+    unset AWS_DEFAULT_PROFILE AWS_PROFILE AWS_EB_PROFILE AWS_PROFILE_REGION
+    echo AWS profile cleared.
+    return
+  fi
+
+  local -a available_profiles
+  available_profiles=($(aws_profiles))
+  if [[ -z "${available_profiles[(r)$1]}" ]]; then
+    echo "${fg[red]}Profile '\''$1'\'' not found in '\''${AWS_CONFIG_FILE:-$HOME/.aws/config}'\''" >&2
+    echo "Available profiles: ${(j:, :)available_profiles:-no profiles found}${reset_color}" >&2
+    return 1
+  fi
+
+  export AWS_DEFAULT_PROFILE=$1
+  export AWS_PROFILE=$1
+  export AWS_EB_PROFILE=$1
+
+  export AWS_PROFILE_REGION=$(aws configure get region)
+
+  if [[ "$2" == "login" ]]; then
+    aws sso login
+  fi
+}
+'
+
+KUBE_TOKEN_GEN_ZSHRC='
+# Generate access token to your clipboard for authenticating into our Kubernetes dashboards
+function kubeToken() {
+  kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '\''{print $1}'\'')  | awk '\''$1=="token:"{print $2}'\'' | pbcopy
+}
+'
 
 # ==========================================================
 # == Workflow-specific configuration                      ==
 # ==========================================================
+
 # The supported workflows. Add to this when more workflows are desired.
 SUPPORTED_WORKFLOWS=("adt")
 
 # --------------------------------------
 # -- ADT workflow configuration       --
 # --------------------------------------
+
+# The directory that repositories are cloned within
 ADT_REPOS_DIR="$REPOS_DIR/adt"
+# The repositories that are cloned
 ADT_WORKFLOW_REPOS=("web" "api" "app-core" "adt-publish-workers")
+# The apps that are installed (via Homebrew)
 ADT_WORKFLOW_HOMEBREW_CASK_APPS=("obs" "hex-fiend" "pgadmin4")
 
 # ==========================================================
@@ -108,7 +167,7 @@ add_text_if_not_exists() {
   local text="$2"
   local token="$3"
 
-  local textToAdd="\n\n$token\n$text"
+  local textToAdd="\n$token$text"
 
   echo "--> Checking if $file already exists"
   if [ ! -e "$file" ]; then
@@ -133,7 +192,7 @@ clone_repo_if_not_exists() {
 
   echo "--> Checking if $repo repository dir already exists."
   if [ ! -d "$ADT_REPOS_DIR/$repo" ]; then
-    echo "--> $repo repository dir does not exist; cloning."
+    echo "[i] $repo repository dir does not exist; cloning."
     git clone $GITHUB_REPOS_SSH_PREFIX/$repo
   else
     echo "--> $repo repository dir already exists; skipping."
@@ -154,7 +213,7 @@ install_homebrew_cask_package() {
   local package="$1"
   
   if brew list "$package" >/dev/null 2>&1; then
-    echo "--> App '$package' is already installed; skipping."
+    echo "[i] App '$package' is already installed; skipping."
   else
     echo "--> App '$package' is not installed; installing."
     if [ "$DISABLE_LONG_RUNNERS" != "1" ]; then
@@ -170,7 +229,7 @@ install_homebrew_non_cask_package() {
   local package="$1"
   
   if brew list "$package" >/dev/null 2>&1; then
-    echo "--> App '$package' is already installed; skipping."
+    echo "[i] App '$package' is already installed; skipping."
   else
     echo "--> App '$package' is not installed; installing."
     if [ "$DISABLE_LONG_RUNNERS" != "1" ]; then
@@ -202,9 +261,23 @@ install_homebrew_cask_packages() {
 supported_workflows_str=$(concatenate_strings "${SUPPORTED_WORKFLOWS[@]}")
 USAGE="Usage: sh setup.sh <workflow>
 
-<workflow> - Optional workflow to enable. Possible values: $supported_workflows_str
+<workflow>                Optional workflow to enable. Possible values: $supported_workflows_str
 
-sh setup.sh --help - show this usage help text"
+sh setup.sh --help        show this usage help text
+sh setup.sh --version     show version"
+
+# Print version screen and exit if first param is "version"-like
+if [ "$1" == "--version" ] || [ "$1" == "-v" ] || [ "$1" == "version" ] || [ "$1" == "v" ]; then
+  echo "$VERSION"
+  exit 0
+fi
+
+# Print splash
+echo
+echo "----------------------------------------"
+echo "--        Vortexa Setup Utility       --"
+echo "----------------------------------------"
+echo
 
 # Print help screen and exit if first param is "help"-like
 if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$1" == "help" ] || [ "$1" == "h" ]; then
@@ -221,10 +294,13 @@ if [ ! -z "$1" ]; then
     exit 1
   fi
 else
-  echo "WARN: <workflow> argument has not been supplied. This means a general-only setup will be performed."
-  echo "      Run with '--help' for more information"
+  echo "------------------------------------------------------------"
+  echo "[!] <workflow> argument has not been supplied."
+  echo "    This means a general-only setup will be performed."
+  echo "    Run with '--help' for more information."
   echo
-  echo "      If this is okay, press 'Enter' to proceed, or any other character to abort."
+  echo "    If this is okay, press 'Enter' to proceed. Else, any other character to abort."
+  echo "------------------------------------------------------------"
   read -n 1 input
   if [[ $input != "" ]]; then
     echo "Exiting."
@@ -259,7 +335,9 @@ add_text_if_not_exists "$SSH_DIR/config" "Host github.com\n  AddKeysToAgent yes\
 
 # Prompt user to add their ssh pub key to their GitHub account, if not already.
 cat "$SSH_KEY_FILE.pub" | pbcopy
+echo "------------------------------------------------------------"
 echo "[i] Your public ssh key has been copied to your clipboard.\n    If you have not already, please add it to your GitHub account at https://github.com/settings/ssh/new.\n    Press 'Enter' to open URL and proceed, or any other key to skip..."
+echo "------------------------------------------------------------"
 read -n 1 input
 
 if [[ $input == "" ]]; then
@@ -270,10 +348,13 @@ else
   echo "[i] Skipping..."
 fi
 
-# Ensure that Homebrew is installed
+# ==========================================================
+# == Homebrew installation                                ==
+# ==========================================================
+
 echo "\n==> Ensuring Homebrew is installed."
 if command -v "brew" >/dev/null 2>&1; then
-  echo "--> \"brew\" command is available; skipping installation."
+  echo "[i] \"brew\" command is available; skipping installation."
 else
   echo "--> \"brew\" command unavailable; installing Homebrew (if a password prompt appears, enter your user's password for your machine.)."
   # From https://brew.sh/
@@ -283,7 +364,7 @@ else
   echo "--> Running \"Next Steps\" to finalize installation."
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> "$HOME/.zprofile"
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  source ~/.zprofile
+  source "$HOME/.zprofile"
 
   # Check that the `brew` command is available and gives us a Homebrew version with the -v arg.
   echo "--> Checking if Homebrew was installed successfully."
@@ -296,24 +377,46 @@ else
   fi
 fi
 
-# Create top-level repos dir if it doesn't exist
+# ==========================================================
+
+# Ensure top-level repos dir exists
 ensure_dir_exists "$REPOS_DIR" "Top-level repos"
 
-# Install general apps via Homebrew
+echo "\n==> Ensuring Rosetta is installed. This may prompt you for your machine user's password."
+echo "------------------------------------------------------------"
+# Ensure rosetta is installed (currently required by aws-vpn-client app)
+sudo softwareupdate --install-rosetta
+
+# Ensure general apps are installed (via Homebrew)
 echo "\n==> Installing general apps via Homebrew."
 install_homebrew_cask_packages "${GENERAL_HOMEBREW_CASK_APPS[@]}"
 install_homebrew_non_cask_packages "${GENERAL_HOMEBREW_NON_CASK_APPS[@]}"
 
-# Add general aliases to .zprofile
-echo "\n==> Ensuring .zprofile has general aliases."
-add_text_if_not_exists "$HOME/.zprofile" "$GENERAL_ALIASES" "$ADDED_BY_US_TOKEN"
-echo "--> Sourcing .zprofile file."
-source "$HOME/.zprofile"
+# Ensure general aliases and other .zshrc content is added
+echo "\n==> Ensuring .zshrc has general aliases and asp command."
+add_text_if_not_exists "$HOME/.zshrc" "$GENERAL_ALIASES_ZSHRC\n\n$ASP_COMMAND_ZSHRC\n\n$KUBE_TOKEN_GEN_ZSHRC" "$ADDED_BY_US_TOKEN"
+echo "--> Sourcing .zshrc file."
+source "$HOME/.zshrc"
 
-# Set default browser
-# First try defaultbrowser Homebrew package method. If that fails, try `open` command method.
+# Ensure NVM is installed (their install script is idempotent out-of-the-box)
+echo "\n==> Ensuring Node Version Manager (nvm) is installed."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+echo "--> Sourcing .zshrc file."
+source "$HOME/.zshrc"
+# Check that the `nvm` command is available and gives us a Homebrew version with the -v arg.
+echo "--> Checking if NVM was installed successfully."
+nvm -v
+if [ $? -eq 0 ]; then
+  echo "[i] NVM installed successfully."
+else
+  echo "[i] NVM not installed successfully; exiting."
+  exit 1
+fi
+
+# Set default browser to Google Chrome
 echo "\n==> Setting Chrome to default browser."
-(echo "--> Installing defaultbrowser Homebrew package." && brew install defaultbrowser && echo "--> Changing default browser." && defaultbrowser chrome) || (echo "--> defaultbrowser tool method failed; trying open command method." && open -a "Google Chrome" --args --make-default-browser)
+# First try defaultbrowser Homebrew package. If that fails, try `open` command instead.
+(install_homebrew_non_cask_package "defaultbrowser" && echo "--> Changing default browser." && defaultbrowser chrome) || (echo "--> defaultbrowser tool failed; trying open command method instead." && open -a "Google Chrome" --args --make-default-browser)
 
 # ==========================================================
 # == Workflow-specific operations                         ==
@@ -336,4 +439,10 @@ if [ $WORKFLOW == "adt" ]; then
   install_homebrew_cask_packages "${ADT_WORKFLOW_HOMEBREW_CASK_APPS[@]}"
 fi
 
-echo "\n\n[i] Done! Happy hacking :-)"
+echo ""
+echo "[i] Note: some commands may only be available after you restart your terminal."
+echo ""
+echo "----------------------------------------"
+echo "--       Done! Happy hacking :-)      --"
+echo "----------------------------------------"
+echo ""
